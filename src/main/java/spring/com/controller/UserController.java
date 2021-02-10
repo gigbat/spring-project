2bded4e9dfaa176e3881236e1db7ffb878bc4be1
gigbat <b.kostevich@gmail.com>
2021-02-10 16:55:50 +0200

    Fixed some mistakes

--- src/main/java/spring/com/controller/UserController.java
+++ src/main/java/spring/com/controller/UserController.java
@@ -3,6 +3,7 @@ package spring.com.controller;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -23,7 +24,7 @@ public class UserController {
             new AnnotationConfigApplicationContext(AppConfig.class);
     private static final UserService userService = context.getBean(UserService.class);
 
-    @RequestMapping(value = "/{userId}", method = RequestMethod.GET)
+    @GetMapping(value = "/{userId}")
     public UserResponseDto getUser(@PathVariable Long userId) {
         Optional<User> optionalUser = userService.get(userId);
         User user = null;
@@ -34,7 +35,7 @@ public class UserController {
         throw new RuntimeException("User " + user + " isn't exist");
     }
 
-    @GetMapping(value = "/inject")
+    @GetMapping(value = "/create")
     public void addUser(@RequestParam String name, @RequestParam String surname,
                         @RequestParam String email) {
         User user = new User(name, surname, email);
@@ -43,11 +44,9 @@ public class UserController {
 
     @GetMapping
     public List<UserResponseDto> getAll() {
-        List<User> users = userService.listUsers();
-        List<UserResponseDto> dtoUsers = new ArrayList<>();
-        for (User user : users) {
-            dtoUsers.add(UserMapperServiceImpl.dtoBoxing(user));
-        }
-        return dtoUsers;
+        return userService.listUsers()
+                .stream()
+                .map(UserMapperServiceImpl::dtoBoxing)
+                .collect(Collectors.toList());
     }
 }
