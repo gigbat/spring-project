137d761c3d9844e7f71492f45ecde63455b70f24
gigbat <b.kostevich@gmail.com>
2021-02-10 17:10:12 +0200

    Done return type when we added user

--- src/main/java/spring/com/controller/UserController.java
+++ src/main/java/spring/com/controller/UserController.java
@@ -7,7 +7,6 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import spring.com.config.AppConfig;
 import spring.com.dto.UserResponseDto;
